91a7481ceb6bb874f7a2959edefdc6da6108e6b2
hafidz-annur <hafidz.bdt@gmail.com>
2021-10-13 15:06:45 +0700

    first init

---
+++ resources/views/layout/module/sidebar-dm.blade.php
@@ -0,0 +1,18 @@
+<style>
+    .list-group-item {
+        padding: 2% 2% 2% 57% !important;
+    }
+
+</style>
+<ul class="list-group" id="sidebar-list">
+    <li class="list-group-item">
+        <a class="sidebar-elements" href="#">
+            Part 1
+        </a>
+    </li>
+    <li class="list-group-item">
+        <a class="sidebar-elements" href="#">
+            Part 2
+        </a>
+    </li>
+</ul>
---
+++ resources/views/modules/digital-marketing/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.module.app')
+@section('title')
+    Digital Marketing
+@endsection
+
+@section('banner')
+    <h1>Image</h1>
+@endsection
+
+@section('sidebar')
+    @include('layout.module.sidebar-dm')
+@endsection
+
+@section('content')
+    test
+@endsection
