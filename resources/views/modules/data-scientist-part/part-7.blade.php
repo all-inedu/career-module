3380397dbd7f63109c5e0023ccd42a55fbbc32b4
Manuel Eric <manuel.eric.b@gmail.com>
2021-10-14 09:57:56 +0700

    update 1.0

--- resources/views/modules/data-scientist-part/part-7.blade.php
+++ resources/views/modules/data-scientist-part/part-7.blade.php
@@ -8,26 +8,29 @@
             </h2>
         </div>
 
-        <ul>
-            <li><a
-                    href="https://www.coursera.org/learn/open-source-tools-for-data-science?specialization=introduction-data-science">Tools
-                    for Data Science</a></li>
-            <li><a href="https://www.coursera.org/learn/data-science-methodology?specialization=introduction-data-science">Data
-                    Science Methodology</a></li>
-            <li><a href="https://www.coursera.org/learn/sql-data-science?specialization=introduction-data-science">Databases
-                    and SQL for Data Science with Python</a></li>
-            <li><a href="https://www.coursera.org/learn/data-cleaning">Getting and Cleaning Data</a></li>
-            <li><a href="https://www.coursera.org/learn/data-visualization-tableau?specialization=data-visualization">Fundamentals
-                    of Visualization with Tableau</a></li>
-            <li><a href="https://www.edx.org/course/data-science-method">The Data Science Method</a></li>
-            <li><a href="https://www.edx.org/course/foundations-of-data-science-prediction-and-machine">Data Science:
-                    Machine Learning and Predictions</a></li>
-            <li><a
-                    href="https://www.analyticsvidhya.com/blog/2018/11/data-engineer-comprehensive-list-resources-get-started/">Data
-                    Engineering</a></li>
-            <li><a href="https://www.datapine.com/blog/data-analysis-methods-and-techniques/">Data Analysis</a></li>
-            <li><a href="https://www.futurelearn.com/info/blog/what-is-machine-learning-a-beginners-guide">Machine
-                    Learning</a></li>
-        </ul>
+        <div class="pt-4">
+            <ul class="ul-spacing">
+                <li><a
+                        href="https://www.coursera.org/learn/open-source-tools-for-data-science?specialization=introduction-data-science">Tools
+                        for Data Science</a></li>
+                <li><a
+                        href="https://www.coursera.org/learn/data-science-methodology?specialization=introduction-data-science">Data
+                        Science Methodology</a></li>
+                <li><a href="https://www.coursera.org/learn/sql-data-science?specialization=introduction-data-science">Databases
+                        and SQL for Data Science with Python</a></li>
+                <li><a href="https://www.coursera.org/learn/data-cleaning">Getting and Cleaning Data</a></li>
+                <li><a href="https://www.coursera.org/learn/data-visualization-tableau?specialization=data-visualization">Fundamentals
+                        of Visualization with Tableau</a></li>
+                <li><a href="https://www.edx.org/course/data-science-method">The Data Science Method</a></li>
+                <li><a href="https://www.edx.org/course/foundations-of-data-science-prediction-and-machine">Data Science:
+                        Machine Learning and Predictions</a></li>
+                <li><a
+                        href="https://www.analyticsvidhya.com/blog/2018/11/data-engineer-comprehensive-list-resources-get-started/">Data
+                        Engineering</a></li>
+                <li><a href="https://www.datapine.com/blog/data-analysis-methods-and-techniques/">Data Analysis</a></li>
+                <li><a href="https://www.futurelearn.com/info/blog/what-is-machine-learning-a-beginners-guide">Machine
+                        Learning</a></li>
+            </ul>
+        </div>
     </div>
 @stop
