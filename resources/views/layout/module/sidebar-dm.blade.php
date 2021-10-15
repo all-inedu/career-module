a9ae9ab6bda09b38222af8f91f0e5007e1f09917
hafidz-annur <hafidz.bdt@gmail.com>
2021-10-15 15:29:05 +0700

    new 15/10/2021

--- resources/views/layout/module/sidebar-dm.blade.php
+++ resources/views/layout/module/sidebar-dm.blade.php
@@ -1,22 +1,22 @@
 <ul class="list-group" id="sidebar-list">
     <li class="list-group-item {{ Request::is('digital-marketing/part-1') ? 'active' : '' }}">
         <a class="sidebar-elements" href="{{ url('digital-marketing/part-1') }}">
-            Part 1 <i class="" id="iconPart1"></i>
+            Intro <i class="" id="iconPart1"></i>
         </a>
     </li>
     <li class="list-group-item {{ Request::is('digital-marketing/part-2') ? 'active' : '' }}">
         <a class="sidebar-elements" href="{{ url('digital-marketing/part-2') }}">
-            Part 2 <i class="" id="iconPart2"></i>
+            Core Tasks <i class="" id="iconPart2"></i>
         </a>
     </li>
     <li class="list-group-item {{ Request::is('digital-marketing/part-3') ? 'active' : '' }}">
         <a class="sidebar-elements" href="{{ url('digital-marketing/part-3') }}">
-            Part 3 <i class="" id="iconPart3"></i>
+            Type <i class="" id="iconPart3"></i>
         </a>
     </li>
     <li class="list-group-item {{ Request::is('digital-marketing/part-4') ? 'active' : '' }}">
         <a class="sidebar-elements" href="{{ url('digital-marketing/part-4') }}">
-            Part 4 <i class="" id="iconPart4"></i>
+            Pathway <i class="" id="iconPart4"></i>
         </a>
     </li>
     <li class="list-group-item {{ Request::is('digital-marketing/part-5') ? 'active' : '' }}">
