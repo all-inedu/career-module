0d7420808bc29f424cc69b5badbc10ed3848056f
Manuel Eric <manuel.eric.b@gmail.com>
2021-10-18 14:26:13 +0700

    minor changes

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -122,7 +122,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #f7f7f7;">
         <div class="container my-1">
-            <a class="navbar-brand" href="#"><img src="{{ asset('img/logo-1.png') }}" width="30%" /></a>
+            <a class="navbar-brand" href="{{ url('/') }}"><img src="{{ asset('img/logo-1.png') }}" width="30%" /></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
