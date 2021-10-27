d5d3b06ce8fca7e021899707c11759c99a7663a8
ManuelEric <manuel.eric.b@gmail.com>
2021-10-27 14:50:32 +0700

    update 27102021

--- resources/views/modules/data-scientist-part/banner.blade.php
+++ resources/views/modules/data-scientist-part/banner.blade.php
@@ -1,4 +1,4 @@
-<div class="banner">
+{{-- <div class="banner">
     <div class="container">
         <h1 class="title">
             Data Scientist <br>
@@ -16,4 +16,20 @@
         <br><br>
         Happy learning!
     </p>
+</div> --}}
+
+<div class="banner">
+    <h1 class="title">
+        Data Scientist <br>
+        101
+    </h1>
 </div>
+<p class="sp mt-5 text-center px-5">
+    This module consists of all the <b>background information</b> on <b>Data Scientist</b> as a career, 2 (two)
+    <b>case
+        studies</b> and a
+    final <b>reflection</b>. Complete all sections <b>in order</b>. Refer to Glossary and Additional Resources when
+    necessary
+    <br><br>
+    Happy learning!
+</p>
