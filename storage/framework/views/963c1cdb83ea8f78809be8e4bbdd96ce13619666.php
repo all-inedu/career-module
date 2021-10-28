85a83e0ba42576d763bc562d4e227488e86ee076
hafidz-annur <hafidz.bdt@gmail.com>
2021-10-28 15:26:20 +0700

    29/10/2021

--- storage/framework/views/963c1cdb83ea8f78809be8e4bbdd96ce13619666.php
+++ storage/framework/views/963c1cdb83ea8f78809be8e4bbdd96ce13619666.php
@@ -32,6 +32,16 @@
                         <b>Let’s walk on your career exploration journey with Career Exploration Module!</b>
                     </p>
                 </div>
+
+                <div class="text-center">
+                    <a href="javascript:void(0)" onclick="show_guidebook()">
+                        <button class="btn btn-allin">Start Your Journey</button>
+                    </a>
+                </div>
+
+                <div class="sp text-center my-5">
+                    <h1 id="career"><b>Career Choices</b></h1>
+                </div>
             </div>
         </div>
 
@@ -48,7 +58,8 @@
                         <a href="<?php echo e(url('digital-marketing/part-1')); ?>">
                             <div class="card border-allin">
                                 <div class="container2 img-bg">
-                                    <img src="<?php echo e(asset('img/icon/CMW_1. Digital Marketing.webp')); ?>" class="card-img-top">
+                                    <img src="<?php echo e(asset('img/icon/CMW_1. Digital Marketing.webp')); ?>"
+                                        class="card-img-top">
                                     <div class="bottom-right">Digital Marketing <br>Specialist
                                     </div>
                                     <i class="fas fa-check-circle text-success float-end mt-1 d-none icon-done" id="dm"></i>
@@ -205,7 +216,6 @@
                 sessionStorage.setItem('first', 'clicked')
             })
         })
-
     </script>
 <?php $__env->stopSection(); ?>
 
